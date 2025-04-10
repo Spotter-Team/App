d13d989a4f6cb69e42c3377c83cbaa061d719aad
maria <mjsel.reynolds@ufl.edu>
2025-04-10 19:25:54 -0400

    Added CommunityType.js

---
+++ backend/src/models/CommunityType.js
@@ -0,0 +1,34 @@
+const { Sequelize, DataTypes, Model } = require('sequelize');
+const dbPath = process.env.LOCAL_DB_PATH;
+
+if (!dbPath) {
+    console.error(`Error: Variable LOCAL_DB_PATH was not found .env file!`);
+    process.exit(1);
+}
+
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: this.dbFilePath
+});
+
+class CommunityType extends Model {}
+
+CommunityType.init({
+    typeID: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    typeName: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    typeDescription: {
+        type: DataTypes.TEXT,
+    }
+}, {
+    sequelize: sequelize,
+    modelName: 'CommunityType'
+});
+
+module.exports = CommunityType;
