4a1041e64f72351c77be1c38d9023a2775b95115
johanfortus <fortusjohan@gmail.com>
2025-04-12 23:39:32 -0400

    Add: Header settings & filter buttons

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,46 @@
 import React from 'react';
 import { View, Text, StyleSheet, Image, SafeAreaView } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import DumbbellLogo from '../assets/dumbbell-logo.png';
 
 const Header = ({ activeScreen }) => {
+
+    const renderContent = () => {
+        if(activeScreen === 'Profile') {
+            return (
+                <View style={styles.buttonContainer}>
+                    <Text style={styles.buttonText}>
+                        Settings
+                    </Text>
+                    <View style={styles.buttonIconContainer}>
+                        <Ionicons name="settings" size={16} color={'white'}></Ionicons>
+                    </View>
+                </View>
+            );
+        }
+        else if(activeScreen === 'Matching') {
+            return (
+                <View style={styles.buttonContainer}>
+                    <Text style={styles.buttonText}>
+                        Filter
+                    </Text>
+                    <View style={styles.buttonIconContainer}>
+                        <Ionicons name="options" size={16} color={'white'}></Ionicons>
+                    </View>
+                </View>
+            );
+        };
+    };
+
     return (
         <SafeAreaView style={styles.safeArea}>
+
             <View style={styles.container}>
                 <View style={styles.logoContainer}>
                     <Image source={DumbbellLogo} style={styles.logo} />
                     <Text style={styles.title}>Spotter</Text>
                 </View>
+                {renderContent()}
             </View>
         </SafeAreaView>
     );
@@ -21,9 +52,9 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         paddingTop: 3,
         paddingBottom: 10,
+        justifyContent: 'space-between',
     },
     logoContainer: {
-        flex: 1,
         flexDirection: 'row',
         paddingLeft: 20,
     },
@@ -39,8 +70,18 @@ const styles = StyleSheet.create({
         paddingLeft: 7,
         fontWeight: 700,
     },
-    settings: {
-        color: 'white'
+    buttonContainer: {
+        flexDirection: 'row',
+        paddingTop: 2,
+        paddingRight: 20,
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: 17
+    },
+    buttonIconContainer: {
+        paddingTop: 3,
+        paddingLeft: 5,
     },
     safeArea: {
         backgroundColor: '#141417'
