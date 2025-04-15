36b2f20e2aceb84cddb82be462c3543a1f854a44
johanfortus <fortusjohan@gmail.com>
2025-04-14 22:27:24 -0400

    Add: Chat screen text input

--- frontend/src/screens/Chats/Chat.js
+++ frontend/src/screens/Chats/Chat.js
@@ -1,4 +1,5 @@
-import { View, Text, Image, StyleSheet, SafeAreaView, ScrollView } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Image, TextInput, StyleSheet, SafeAreaView, ScrollView, KeyboardAvoidingView } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import ChatHeader from '../../components/ChatsComponents/ChatHeader';
@@ -7,6 +8,9 @@ import ChatBubbleReceiver from '../../components/ChatsComponents/ChatBubbleRecei
 
 
 const Chat = () => {
+
+    const [value, onChangeText] = useState('Type a message ...')
+
     return (
         <SafeAreaView style={styles.safeArea}>
 
@@ -17,10 +21,29 @@ const Chat = () => {
 
                 <ChatBubbleSender />                
                 <ChatBubbleReceiver />
-
+                
             </ScrollView>
 
-            
+            <View style={styles.inputContainer}>
+                <TextInput
+                    multiline
+                    maxLength={200}
+                    style={styles.input}
+                    placeholder='Type a message...'
+                    placeholderTextColor={'#737373'}
+                    onChangeText={text => onChangeText(text)}
+                >
+                </TextInput>
+
+                <Text style={styles.sendBtn}>
+                    SEND
+                </Text>
+            </View>
+
+            <View style={styles.inputFooter}>
+                <Ionicons name="images-outline" size={25} style={styles.footerIcon}></Ionicons>
+                <Ionicons name="camera-outline" size={25} style={styles.footerIcon}></Ionicons>
+            </View>
 
         </SafeAreaView>
     );
@@ -34,6 +57,41 @@ const styles = StyleSheet.create({
     chatArea: {
         backgroundColor: 'black',
     },
+    inputContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'flex-end',
+        paddingVertical: 15,
+        paddingLeft: 20,
+        backgroundColor: '#141417',
+        color: 'white',
+        borderBottomWidth: 1,
+        borderBottomColor: '#3C444F',
+        borderTopColor: '#3C444F',
+        borderTopWidth: 1,
+    },
+    input: {
+        color: 'white',
+        fontSize: 16,
+        width: '80%',
+    },
+    sendBtn: {
+        marginRight: 20,
+        color: '#B42B23',
+        fontSize: 16,
+        fontWeight: 600,
+        width: 45,
+    },
+    inputFooter: {
+        flexDirection: 'row',
+        backgroundColor: '#141417',
+        paddingLeft: 10,
+        paddingTop: 10,
+    },
+    footerIcon: {
+        color: "#737373",
+        paddingLeft: 10,
+    }
 });
 
 export default Chat;
