50a44060efe1772aee01acbced510d9021ff85e4
Brandon Rosenthal <brosenthal140@ufl.edu>
2025-04-14 20:50:09 -0400

    - Refactor: adds other models to schema.js

--- backend/src/models/schema.js
+++ backend/src/models/schema.js
@@ -2,6 +2,19 @@ const { userSchema, User} = require('./User');
 const { availabilitySchema, Availability } = require('./Availability');
 const { blockedSchema, Blocked } = require('./Blocked');
 const { communitySchema, Community } = require('./Community');
+const { communityTypeSchema, CommunityType } = require('./CommunityType');
+const { directMessageSchema, DirectMessage } = require('./DirectMessage');
+const { goesToSchema, GoesTo } = require('./GoesTo');
+const { gymSchema, Gym } = require('./Gym');
+const { matchSchema, Match } = require('./Match');
+const { meetupSchema, Meetup } = require('./Meetup');
+const { memberOfSchema, MemberOf } = require('./MemberOf');
+const { requiredEquipmentSchema, RequiredEquipment } = require('./RequiredEquipment');
+const { timeSlotSchema, TimeSlot } = require('./TimeSlot');
+const { userReportSchema, UserReport } = require('./UserReport');
+const { userReportTypeSchema, UserReportType } = require('./UserReportType');
+const { workoutSchema, Workout } = require('./Workout');
+const { workoutEquipmentSchema, WorkoutEquipment } = require('./WorkoutEquipment');
 
 module.exports = {
     userSchema,
@@ -11,5 +24,31 @@ module.exports = {
     blockedSchema,
     Blocked,
     communitySchema,
-    Community
+    Community,
+    communityTypeSchema,
+    CommunityType,
+    directMessageSchema,
+    DirectMessage,
+    goesToSchema,
+    GoesTo,
+    gymSchema,
+    Gym,
+    matchSchema,
+    Match, 
+    meetupSchema,
+    Meetup,
+    memberOfSchema,
+    MemberOf,
+    requiredEquipmentSchema,
+    RequiredEquipment,
+    timeSlotSchema,
+    TimeSlot,
+    userReportSchema,
+    UserReport,
+    userReportTypeSchema,
+    UserReportType,
+    workoutSchema,
+    Workout,
+    workoutEquipmentSchema,
+    WorkoutEquipment
 };
