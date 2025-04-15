305a8c0e5436d44de89ee12ddece44c817a96d31
johanfortus <fortusjohan@gmail.com>
2025-04-14 23:02:04 -0400

    Refactor: Chat screen styles

--- frontend/src/screens/Chats/Chat.js
+++ frontend/src/screens/Chats/Chat.js
@@ -1,31 +1,32 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, SafeAreaView, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
+import {
+    View,
+    Text,
+    TextInput,
+    StyleSheet,
+    SafeAreaView,
+    ScrollView,
+    KeyboardAvoidingView,
+    Platform 
+} from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import ChatHeader from '../../components/ChatsComponents/ChatHeader';
 import ChatBubbleSender from '../../components/ChatsComponents/ChatBubbleSender';
 import ChatBubbleReceiver from '../../components/ChatsComponents/ChatBubbleReceiver';
 
-
 const Chat = () => {
 
     const [value, onChangeText] = useState('')
 
     return (
-        <KeyboardAvoidingView style={{ flex: 1, backgroundColor: '#141417' }} behavior={Platform === 'ios' ? 'padding' : 'height'}>
+        <KeyboardAvoidingView style={styles.chatScreen} behavior={Platform === 'ios' ? 'padding' : 'height'} keyboardVerticalOffset={-40}>
             <SafeAreaView style={styles.safeArea}>
-
                 <ChatHeader />
-
                 <ScrollView style={styles.chatArea} contentContainerStyle={{ flex: 1, marginTop: 10 }}>
-
-
-                    <ChatBubbleSender />                
+                    <ChatBubbleSender />
                     <ChatBubbleReceiver />
-                    
                 </ScrollView>
-
-
             </SafeAreaView>
             <View style={styles.inputContainer}>
                 <TextInput
@@ -38,22 +39,21 @@ const Chat = () => {
                     onChangeText={text => onChangeText(text)}
                 >
                 </TextInput>
-
-                <Text style={styles.sendBtn}>
-                    SEND
-                </Text>
+                <Text style={styles.sendBtn}>SEND</Text>
             </View>
-
             <View style={styles.inputFooter}>
-                <Ionicons name="images-outline" size={25} style={styles.footerIcon}></Ionicons>
-                <Ionicons name="camera-outline" size={25} style={styles.footerIcon}></Ionicons>
+                <Ionicons name="images-outline" size={28} style={styles.footerIcon}></Ionicons>
+                <Ionicons name="camera-outline" size={28} style={styles.footerIcon}></Ionicons>
             </View>      
-                  
         </KeyboardAvoidingView>
     );
 };
 
 const styles = StyleSheet.create({
+    chatScreen: {
+        flex: 1,
+        backgroundColor: '#141417',
+    },
     safeArea: {
         flex: 1,
         backgroundColor: '#141417',
@@ -85,13 +85,14 @@ const styles = StyleSheet.create({
         color: '#B42B23',
         fontSize: 16,
         fontWeight: 600,
-        width: 45,
+        width: '20%',
     },
     inputFooter: {
         flexDirection: 'row',
         backgroundColor: '#141417',
         paddingLeft: 10,
         paddingTop: 10,
+        paddingBottom: 10,
     },
     footerIcon: {
         color: "#737373",
