68a2e16dadcd2a18d740b86f9351ab33e9b35cfc
johanfortus <fortusjohan@gmail.com>
2025-04-16 15:31:43 -0400

    Add: Settings header

---
+++ frontend/src/components/SettingsComponents/SettingsHeader.js
@@ -0,0 +1,52 @@
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+
+const SettingsHeader = () => {
+
+    const navigation = useNavigation();
+    const handleBackBtnPress = () => navigation.goBack();
+
+    return (
+        <View style={styles.settingsHeaderContainer}>
+            <TouchableOpacity onPress={() => handleBackBtnPress()}>
+                <Ionicons name="chevron-back" size={25} style={styles.backButton}></Ionicons>                
+            </TouchableOpacity>
+
+            <View style={styles.titleContainer}>
+                <Text style={styles.title}>Settings and Privacy</Text>
+            </View>
+            <View style={styles.placeHolder} ></View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    settingsHeaderContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: '#141417',
+        paddingTop: 3,
+        paddingBottom: 10,
+    },
+    backButton: {
+        color: '#B42B23',
+        paddingLeft: 20,
+    },
+    titleContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        color: 'white',
+        fontSize: 15,
+        fontWeight: 600,
+        paddingRight: 20,
+    },
+    placeHolder: {
+        paddingRight: 20,
+    }
+});
+
+export default SettingsHeader;
