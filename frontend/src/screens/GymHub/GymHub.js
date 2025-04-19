ece5b4fa9535636193b686dae5ef1a7e80f8254b
johanfortus <fortusjohan@gmail.com>
2025-04-18 22:23:28 -0400

    Add: GymHub Tabs

--- frontend/src/screens/GymHub/GymHub.js
+++ frontend/src/screens/GymHub/GymHub.js
@@ -1,4 +1,5 @@
 import { View, Text, Image, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/native';
 import HubCommunityScroll from '../../components/GymHubComponents/HubCommunityScroll';
@@ -8,23 +9,61 @@ const GymHub = () => {
     const navigation = useNavigation();
     const handleViewAllPress = () => navigation.navigate('CommunityList');
 
+    const [ activeTab, setActiveTab ] = useState('feed');
+    
+    const renderContent = () => {
+        if(activeTab === 'feed') {
+            return (
+                <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: 200 }}>
+                    <Text style={{ color: 'white', fontSize: 30 }}>Feed Content</Text>
+                </View>
+            );
+        }
+        else if(activeTab === 'communities') {
+            return (
+                <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: 200 }}>
+                    <Text style={{ color: 'white', fontSize: 30 }}>Communities Content</Text>
+                </View>
+            );
+        };
+    };
+
+    const handleFeedPress = () => {
+        setActiveTab('feed');  
+    };
+
+    const handleCommunitiesPress = () => {
+        setActiveTab('communities');
+    }
 
     return (
         <View style={styles.container}>
-            
             <View style={styles.titleContainer}>
                 <Text style={styles.title}>
                     Gym Hub
                 </Text>
-                <TouchableOpacity onPress={handleViewAllPress}>
+                <TouchableOpacity style={styles.viewAllBtn} onPress={handleViewAllPress}>
                     <Text style={styles.viewAllText}>
                         View All
                     </Text>
                 </TouchableOpacity>
             </View>
-
             <HubCommunityScroll />
             <View style={styles.divider} />
+
+            <View style={styles.tabContainer}>
+                <TouchableOpacity onPress={handleFeedPress}>
+                    <Text style={activeTab === 'feed' ? styles.activeTab : styles.inactiveTab}>My Feed</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={handleCommunitiesPress}>
+                    <Text style={activeTab === 'communities' ? styles.activeTab : styles.inactiveTab}>Communities</Text>
+                </TouchableOpacity>
+                
+            </View>
+
+            {renderContent()}
+
         </View>
     );
 };
@@ -42,6 +81,9 @@ const styles = StyleSheet.create({
         color: 'white',
         fontSize: 26,
         marginLeft: 8,
+    },
+    viewAllBtn: {
+        marginRight: 8,
     },
     viewAllText: {
         color: 'white',
@@ -67,6 +109,30 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-around',
     },
+    feedTab: {
+
+    },
+    myFeedText: {
+        color: 'white',
+        fontFamily: 'Bebas Neue',
+        fontSize: 23,
+    },
+    tabContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginTop: 26,
+        marginHorizontal: 47,
+    },
+    activeTab: {
+        color: '#B42B23',
+        fontFamily: 'Bebas Neue',
+        fontSize: 26,
+    },
+    inactiveTab: {
+        color: 'white',
+        fontFamily: 'Bebas Neue',
+        fontSize: 26,
+    }
 });
 
 export default GymHub;
