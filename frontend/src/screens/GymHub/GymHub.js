729b7980925f62a99f59979b77aadf9013a9ad2d
johanfortus <fortusjohan@gmail.com>
2025-04-20 00:31:07 -0400

    Add: Gym hub feed input

--- frontend/src/screens/GymHub/GymHub.js
+++ frontend/src/screens/GymHub/GymHub.js
@@ -1,9 +1,10 @@
-import { View, Text, Image, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+import { View, Text, Image, StyleSheet, ScrollView, TouchableOpacity, TextInput } from 'react-native';
 import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useNavigation } from '@react-navigation/native';
 import HubCommunityScroll from '../../components/GymHubComponents/HubCommunityScroll';
 import GymHubTabs from '../../components/GymHubComponents/GymHubTabs';
+import { Ionicons } from '@expo/vector-icons';
 
 const GymHub = () => {
 
@@ -11,6 +12,7 @@ const GymHub = () => {
     const handleViewAllPress = () => navigation.navigate('CommunityList');
 
     const [ activeTab, setActiveTab ] = useState('feed');
+    const [ value, onChangeText ] = useState('');
     
     const renderContent = () => {
         if(activeTab === 'feed') {
@@ -24,6 +26,7 @@ const GymHub = () => {
             return (
                 <View style={{ justifyContent: 'center', alignItems: 'center', marginTop: 200 }}>
                     <Text style={{ color: 'white', fontSize: 30 }}>Communities Content</Text>
+                    <View style={styles.inputDivider} />
                 </View>
             );
         };
@@ -47,7 +50,38 @@ const GymHub = () => {
                 <View style={styles.divider} />
                 <GymHubTabs activeTab={activeTab} setActiveTab={setActiveTab} />
             </View>
-            {renderContent()}
+
+            <View style={styles.feedContainer}>
+                <View style={styles.inputContainer}>
+                    <TextInput
+                        multiline
+                        maxLength={200}
+                        style={styles.input}
+                        placeholder="Share your grind!"
+                        placeholderTextColor={'#737373'}
+                        onChangeText={text => onChangeText(text)}
+                    >
+                        
+                    </TextInput>
+                    <View style={styles.inputDivider} />
+
+                    <View style={styles.inputSubmitContainer}>
+                        <TouchableOpacity style={{ flexDirection: 'row' }}>
+                            <Text style={styles.hubText}>
+                                HUB
+                            </Text>
+                            <Ionicons name="chevron-down" size={16} style={styles.postInHubButton} />
+                        </TouchableOpacity>
+
+                        <TouchableOpacity style={styles.publishPostBtn}>
+                            <Text style={styles.publishBtnText}>PUBLISH POST</Text>
+                        </TouchableOpacity>
+
+                    </View>
+
+                </View>
+            </View>
+      
             <Image source={require('../../assets/red-lines-graphic.png')} style={styles.redLines}></Image>
             
             <Image source={require('../../assets/red-dots-graphic.png')} style={styles.redDots}></Image>
@@ -106,6 +140,56 @@ const styles = StyleSheet.create({
         color: 'white',
         fontFamily: 'Bebas Neue',
         fontSize: 23,
+    },
+    feedContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 20,
+    },
+    inputContainer: {
+        backgroundColor: '#0D0E0D',
+        height: 113,
+        width: 340,
+        borderColor: '#222525',
+        borderWidth: 1,
+        borderRadius: 12,
+        paddingHorizontal: 25,
+        paddingVertical: 10,
+        justifyContent: 'space-around',
+    },
+    input: {
+        fontSize: 14,
+        color: 'white',
+    },
+    inputDivider: {
+        height: 2,
+        backgroundColor: '#1F2221',
+    },
+    inputSubmitContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    hubText: {
+        color: 'white',
+        fontFamily: 'Montserrat',
+        fontSize: 14,
+        fontWeight: 800,
+    },
+    postInHubButton: {
+        color: 'white',
+    },
+    publishPostBtn: {
+        backgroundColor: '#9F301C',
+        paddingVertical: 5,
+        paddingHorizontal: 7,
+        borderRadius: 10,
+        borderColor: '#AF331E',
+        borderWidth: 1,
+    },
+    publishBtnText: {
+        color: 'white',
+        fontWeight: 800,
     },
     redLines: {
         width: 120,
