e5ac88ee2c4555098fadec52f7029b04ef655db6
johanfortus <fortusjohan@gmail.com>
2025-04-20 01:11:50 -0400

    Add: Feed post avatar and user information

--- frontend/src/components/GymHubComponents/FeedPost.js
+++ frontend/src/components/GymHubComponents/FeedPost.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
 
 const FeedPost = () => {
     return (
@@ -13,6 +13,16 @@ const FeedPost = () => {
                     </Text>
                 </TouchableOpacity>
             </View>
+
+            <View style={styles.userInfoContainer}>
+                <Image source={require('../../mock/GymHub/feedpfp1.jpg')} style={styles.userAvatar}/>
+
+                <View style={{ paddingLeft: 7 }}>
+                    <Text style={styles.userName}>User Name</Text>
+                    <Text style={styles.userLocation}>Florida, United States</Text>
+                </View>
+            </View>
+
         </View>
     );
 };
@@ -46,6 +56,27 @@ const styles = StyleSheet.create({
     viewHubText: {
         fontSize: 12,
         color: '#9F301C',
+    },
+    userInfoContainer: {
+        flexDirection: 'row',
+        paddingVertical: 12,
+        alignItems: 'center',
+    },
+    userAvatar: {
+        height: 50,
+        width: 50,
+        borderRadius: 10,
+        borderColor: '#9F301C',
+        borderWidth: 2,
+    },
+    userName: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 700,
+    },
+    userLocation: {
+        color: 'white',
+        fontSize: 12,
     }
 });
 
