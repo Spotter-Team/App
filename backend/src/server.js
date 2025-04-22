ae39f0c5059573dc4a71561184bbe18e00726bcc
maria <mjsel.reynolds@ufl.edu>
2025-04-22 13:40:03 -0400

    Updated routes to better fit matching functionality API calls

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 // Require routes
 const userRoutes = require('./routes/User');
 const directMessageRoutes = require('./routes/DirectMessage');
+const matchRoutes = require('./routes/Match');
 
 const app = express();
 const port = 3000;
@@ -17,3 +18,4 @@ app.listen(port, '0.0.0.0', () => {
 // Register routes
 app.use('/api/user', userRoutes);
 app.use('/api/msg', directMessageRoutes);
+app.use('/api/match', matchRoutes);
